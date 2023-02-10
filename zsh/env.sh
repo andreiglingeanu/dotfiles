21780db79e7c9d790103d2beca6bbc410810b580
Andrei Glingeanu <andrei.glingeanu@gmail.com>
2023-02-10 11:34:43 +0200

    cleanup env

--- zsh/env.sh
+++ zsh/env.sh
@@ -1,4 +1,3 @@
-# ENVIRONMENT
 if [ -d "$HOME/bin" ] ; then
   PATH="$PATH:$HOME/bin"
 fi
@@ -7,24 +6,8 @@ if [ -d "$ZSHA_BASE/scripts" ] ; then
   PATH="$PATH:$ZSHA_BASE/scripts"
 fi
 
-if [ -d "$HOME/.linuxbrew/bin" ] ; then
-  PATH="$PATH:$HOME/.linuxbrew/bin"
-fi
-
 if [ -d "/usr/local/bin" ] ; then
   PATH="$PATH:/usr/local/bin"
-fi
-
-if [ -d "/usr/local/smlnj/bin" ] ; then
-  PATH="$PATH:/usr/local/smlnj/bin"
-fi
-
-if [ -d "$HOME/.linuxbrew/share/man" ] ; then
-  export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
-fi
-
-if [ -d "$HOME/.linuxbrew/share/info" ] ; then
-  export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
 fi
 
 if [ -d "$HOME/Dropbox/bin" ] ; then
@@ -39,43 +22,13 @@ if [ -d "$HOME/Projects/full/wp" ]; then
   export WP_DEVELOP_DIR="$HOME/Projects/full/wp/"
 fi
 
-if [ -d "/Applications/DB Browser for SQLite.app/Contents/MacOS" ]; then
-  export PATH="$PATH:/Applications/DB Browser for SQLite.app/Contents/MacOS"
-fi
-
-if [ -f "$HOME/bin/google-cloud-sdk/completion.zsh.inc" ]; then
-	source "$HOME/bin/google-cloud-sdk/completion.zsh.inc"
-fi
-
-if [ -f "$HOME/.asdf/asdf.sh" ]; then
-	# . $HOME/.asdf/asdf.sh
-fi
-
-# if [ -d "/home/linuxbrew/.linuxbrew/bin" ]; then
-	# export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-# fi
-
-if [ -f "$HOME/bin/google-cloud-sdk/path.zsh.inc" ]; then
-	source "$HOME/bin/google-cloud-sdk/path.zsh.inc"
-fi
-
-# export WP_DEVELOP_THEME_NAME="ct"
-
-export WP_TESTS_DIR=/tmp/wordpress-tests-lib/
-
 export PATH=/opt/local/bin:/opt/local/sbin:$PATH
-
-export LANG=ru_RU.UTF-8
 
 export CDPATH=$CDPATH:$HOME/Sites:$HOME
 export PATH=$HOME/bin:/opt/homebrew/bin:$HOME/.composer/vendor/bin:/usr/local/mysql/bin:$PATH
 export VMWWW=/Volumes/www/html
 export VMPHP=/Volumes/www/phpapps
 export EDITOR=nvim
-
-export ANDROID_HOME=${HOME}/Library/Android/sdk
-export PATH=${PATH}:${ANDROID_HOME}/tools
-export PATH=${PATH}:${ANDROID_HOME}/platform-tools
 
 export GOPATH=$HOME/Go
 export GOROOT=/usr/local/opt/go/libexec
@@ -85,7 +38,6 @@ export PATH="/usr/local/opt/gettext/bin:$PATH"
 export GPG_TTY=$(tty)
 
 export NVIM_TUI_ENABLE_TRUE_COLOR=1
-
 
 export PHP_VERSIONS="/usr/local/"
 
